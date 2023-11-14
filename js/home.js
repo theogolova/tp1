31207fa65069b9de1757e4b66ff8573f5e595e9b
Theo Golova <tgolova@udesa.edu.ar>
2023-11-14 09:45:35 -0300

    solucione error en homejs

--- js/home.js
+++ js/home.js
@@ -1,23 +1,28 @@
-let formulario = document.querySelector(".buscador")
-let moviesapi = 'https://api.themoviedb.org/3/movie/now_playing?api_key=aad4ccb8efdd15fad341576d3301e95e'
+let formulario = document.querySelector(".buscador");
+let moviesapi = 'https://api.themoviedb.org/3/movie/now_playing?api_key=aad4ccb8efdd15fad341576d3301e95e';
+
 fetch(moviesapi)
-.then(function(response) {
-    return response.json()
-})
-.then(function(data) {
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
     console.log(data);
-    console.log(data.results)
-    let contenidomovies = data.results
-    let input = documentquerySelector("input")
-    input.addEventListener(" ")
-
-
-
-
-})
-.catch(function(err) {
+    console.log(data.results);
+    
+    let contenidomovies = data.results;
+    
+    // Corrección: Selecciona el elemento input correctamente
+    let input = document.querySelector("input");
+    
+    input.addEventListener("evento", function() {
+      // Aquí puedes poner el código que se ejecutará cuando ocurra el evento en el input
+      console.log("Evento en el input");
+    });
+  })
+  .catch(function(err) {
     console.error(err);
-})
+  });
+
 
 let logofooter = document.getElementById('tmdb-logo');
 
