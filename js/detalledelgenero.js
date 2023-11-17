52335064cc32c8f0d814d804143c4fb090aeb4aa
antoniocaoo <acaolavignolle@udesa.edu.ar>
2023-11-17 18:19:46 -0300

    js det genero

--- js/detalledelgenero.js
+++ js/detalledelgenero.js
@@ -1,3 +1,35 @@
+let apiKey = "fb127d20d8c5df555d204c84bd6bab37";
+let queryString = location.search;
+let genres = new URLSearchParams(queryString);
+let genresTag = genres.get('id');
+
+console.log(genresTag)
+
+fetch(
+  `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.asc&with_genres=${genresTag}`
+)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    let divgeneros = document.getElementById("divgeneros")
+    let peliculasGeneros = data.results;
+    console.log(peliculasGeneros)
+    let pelicula = "";
+
+    for (let i = 1; i < peliculasGeneros.length; i++) {
+      pelicula += `
+                <article class="relac un">
+                    <a href="detallepelicula.html?id=${peliculasGeneros[i].id}" class="detalleGenero" id="${peliculasGeneros[i].id}">${peliculasGeneros[i].title}</a>
+                    <img src="https://image.tmdb.org/t/p/w500/${peliculasGeneros[i].poster_path}">
+                </article>`;
+      
+    }divgeneros.innerHTML = pelicula;
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
 let logofooter = document.getElementById('tmdb-logo');
 
 // Ejemplo: Cambiar el tamaño al pasar el mouse sobre el logo
